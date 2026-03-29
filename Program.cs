9c45fbc5cfd9d047e7c134f179ae47dc8d8af476
santola.developer@gmail.com <santola.developer@gmail.com>
2026-03-29 15:55:30 +0700

    modify folder structure cd

--- Program.cs
+++ Program.cs
@@ -1,33 +1,47 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();
+    app.MapSwagger();
     app.UseSwaggerUI();
 }
 
-// define instance middleware 
 app.UseHttpsRedirection();
 
-var greeting = new[]
+app.MapGet("/product", async (HttpClient httpClient) =>
 {
-    "Hello Sambath pg thom"
-};
+     var respone = await httpClient.GetAsync("https://api.escuelajs.co/api/v1/products");
 
-
-// Define a simple GET route
-app.MapGet("/hello", () => greeting);
-
-
+    if (respone.StatusCode == System.Net.HttpStatusCode.OK)
+    { 
+        var data = await respone.Content.ReadAsStringAsync();
+        return Results.Ok(data);
+    }
+    else
+    {
+       return Results.BadRequest("Faild to load Products");
+    }
+   
+});
 
 app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
