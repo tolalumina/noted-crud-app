082e0c35efd6f1225a180c5ca01b4cc1ddaa9195
santola.developer@gmail.com <santola.developer@gmail.com>
2026-03-29 22:21:45 +0700

    config: folder project structure to on Data NoteDbcontext

--- Program.cs
+++ Program.cs
@@ -1,21 +1,24 @@
+using NotedApp.Api.Data;
 using NotedApp.Api.Models.DTOs;
 using NotedApp.Api.Models.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. ADD SERVICES TO THE CONTAINER
-builder.Services.AddControllers();
+// --- 1. REGISTER SERVICES ---
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// 2. CONFIGURE CORS (Crucial for Vue 3 + Axios)
+// Database Context
+builder.Services.AddSingleton<NotedDbContext>();
+
+// CORS Configuration (Must be registered before builder.Build())
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowVueApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5173") // Default Vue 3 port
+        policy.WithOrigins("http://localhost:5173") 
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
@@ -23,22 +26,23 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// 3. CONFIGURE THE HTTP REQUEST PIPELINE
+// --- 2. CONFIGURE HTTP PIPELINE ---
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Redirect HTTP to HTTPS
 app.UseHttpsRedirection();
 
-// Apply the CORS policy BEFORE mapping controllers
+// Middleware Order: Routing -> CORS -> Auth -> Endpoints
+app.UseRouting();
+
 app.UseCors("AllowVueApp");
 
 app.UseAuthorization();
 
-// This line "calls" the controllers based on the [Route] attributes
 app.MapControllers();
 
 app.Run();
